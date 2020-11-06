f5e1e49dd319a07b8f103a9b7b7984d282c282ae
Marek Klatt <531devv@gmail.com>
2020-11-06 22:35:05 +0100

    Main files

---
+++ src/components/TodoApp.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <input type="text" placerholder="todo" v-model="newItem" />
+    <button @click="addItem">Dodaj</button>
+    <TodoItem v-bind:item="item" v-for="item in items" v-bind:key="item.id" @removeClicked = "removeItem" />
+  </div>
+</template>
+
+<script>
+
+import TodoItem from './TodoItem.vue';
+export default {
+  data() {
+    return {
+      newItem: '',
+      items: [
+        {id: 1, title: 'Zrobić zakupy', completed: false},
+        {id: 2, title: 'Zrobić cos', completed: true}
+      ]
+    }
+  },
+  components: {
+      TodoItem
+  },
+  methods: {
+    removeItem(id) {
+      const index = this.items.findIndex(el => el.id === id);
+      this.items[index].completed = true;
+    },
+    addItem() {
+      this.items.push({
+        title: this.newItem,
+        completed: false,
+        id: Math.random()
+      })
+    }
+  },
+}
+</script>
+
+<style>
+.completed {
+  text-decoration: line-through;
+  color: rgb(70, 70, 70);
+}
+</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,17 @@
+<template>
+    <div v-bind:class="{completed: item.completed}">
+      <button v-if=(!item.completed) @click="removeItem(item.id)">Usuń</button>
+      <h2>{{ item.title }}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['item'],
+    methods: {
+        removeItem(id) {
+            this.$emit('removeClicked', id);
+        }
+    }
+}
+</script>
